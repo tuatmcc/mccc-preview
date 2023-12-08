cf3fa41303f80acd3f7b5f7233fb151eeaed1d05
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-12-08 15:41:22 +0900

    add controllers and lights

---
+++ src/app/mccc/_components/Lights/index.jsx
@@ -0,0 +1,103 @@
+import React, { useRef } from 'react'
+import * as THREE from 'three'
+import { useControls } from 'leva'
+
+export const Lights = () => {
+  const ambientRef = useRef()
+  const directionalRef = useRef()
+  const pointRef = useRef()
+  const spotRef = useRef()
+
+  useControls('Ambient Light', {
+    visible: {
+      value: false,
+      onChange: (v) => {
+        ambientRef.current.visible = v
+      },
+    },
+    color: {
+      value: 'white',
+      onChange: (v) => {
+        ambientRef.current.color = new THREE.Color(v)
+      },
+    },
+  })
+
+  useControls('Directional Light', {
+    visible: {
+      value: true,
+      onChange: (v) => {
+        directionalRef.current.visible = v
+      },
+    },
+    position: {
+      x: 1,
+      y: 1,
+      z: 1,
+      onChange: (v) => {
+        directionalRef.current.position.copy(v)
+      },
+    },
+    color: {
+      value: 'white',
+      onChange: (v) => {
+        directionalRef.current.color = new THREE.Color(v)
+      },
+    },
+  })
+
+  useControls('Point Light', {
+    visible: {
+      value: false,
+      onChange: (v) => {
+        pointRef.current.visible = v
+      },
+    },
+    position: {
+      x: 2,
+      y: 0,
+      z: 0,
+      onChange: (v) => {
+        pointRef.current.position.copy(v)
+      },
+    },
+    color: {
+      value: 'white',
+      onChange: (v) => {
+        pointRef.current.color = new THREE.Color(v)
+      },
+    },
+  })
+
+  useControls('Spot Light', {
+    visible: {
+      value: false,
+      onChange: (v) => {
+        spotRef.current.visible = v
+      },
+    },
+    position: {
+      x: 3,
+      y: 2.5,
+      z: 1,
+      onChange: (v) => {
+        spotRef.current.position.copy(v)
+      },
+    },
+    color: {
+      value: 'white',
+      onChange: (v) => {
+        spotRef.current.color = new THREE.Color(v)
+      },
+    },
+  })
+
+  return (
+    <>
+      <ambientLight ref={ambientRef} />
+      <directionalLight ref={directionalRef} />
+      <pointLight ref={pointRef} />
+      <spotLight ref={spotRef} />
+    </>
+  )
+}
--- src/app/mccc/page.jsx
+++ src/app/mccc/page.jsx
@@ -3,6 +3,7 @@
 import { Canvas, useLoader } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { Stats, OrbitControls } from '@react-three/drei';
+import { Lights } from './_components/Lights';
 
 const Model = () => {
   const gltf = useLoader(GLTFLoader, '/models/mccc.gltf');
@@ -10,11 +11,16 @@ const Model = () => {
 };
 
 export default function Page() {
+
+
   return (
     <div className="h-screen">
       <Canvas>
+        <Lights />
+        <pointLight position={[0, 5, 10]} intensity={5} />
         <Model />
         <OrbitControls />
+        <axesHelper args={[5]} />
         <Stats />
       </Canvas>
     </div>
